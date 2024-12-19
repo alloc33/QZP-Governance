49c1f547998c6a1c37d21fa4259b9a4f8f5c60f2
nshv <nickshv13@icloud.com>
2024-12-19 14:11:13 +0700

    Update token id

--- programs/qzl_token/src/lib.rs
+++ programs/qzl_token/src/lib.rs
@@ -7,7 +7,7 @@ pub use instructions::*;
 pub use utils::*;
 
 // Declare the program ID to associate this Rust program with the deployed Solana program.
-declare_id!("4wYyKpdmL82mehsRdiTCTbCnGCBZjdgbz6vE8J46VQFn");
+declare_id!("4avjKuLNY7Q2PswouSSDGm5rETTqf1t6MA4bMfUjcRjH");
 
 // Define a constant for the administrator's public key.
 // This key is used to authenticate administrative actions within the governance contract.
